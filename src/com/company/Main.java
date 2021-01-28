98a2efa2eeffd73d722c8b3c5c788e2fbe3ca375
MujtabaHafizi <muj.bdn@gmail.com>
2021-01-27 21:51:45 -0500

    Solar System Specifications Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // we are going to creating a solar system
+        SolarSystem FactorX = new SolarSystem();
+
+        int sun= 1;
+        int planets = 3;
+        int Moons = 2;
+
+    }
+}
